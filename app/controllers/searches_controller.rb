dd212fd832ef29fd339c78d31793d86021b5f6d3
Daniel Valmas <daniel.valmas@gmail.com>
2018-10-16 21:53:06 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,16 +8,8 @@ class SearchesController < ApplicationController
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
-          body = JSON.parse(@resp.body)
-      if @resp.success?
-        @venues = body["response"]["venues"]
-      else
-        @error = body["meta"]["errorDetail"]
-      end
- 
-    rescue Faraday::ConnectionFailed
-      @error = "There was a timeout. Please try again."
-    end
+     body_hash = JSON.parse(@resp.body)
+    @venues = body_hash["response"]["venues"]
     render 'search'
   end 
   
